11d7f5905220ff29481f18f2c12c0f35082210a9
Dee Macbook <ce_dalalalrasheedi@hotmail.com>
2020-08-30 18:20:08 +0300

    added tripstore

---
+++ stores/TripStore.js
@@ -0,0 +1,30 @@
+import { decorate, observable } from "mobx";
+import instance from "./instance";
+
+class TripStore {
+  trips = [];
+  loading = true;
+
+  fetchTrips = async () => {
+    //error handler
+    try {
+      const response = await instance.get("/trips");
+      // console.log("NailStore -> fetchItems -> error", response); // test to see where data come from
+      this.trips = response.data;
+      this.loading = false;
+    } catch (error) {
+      console.error("TripStore -> fetchTrips -> error", error);
+    }
+  };
+
+  getTripById = (tripId) => this.trips.find((trip) => trip.id === tripId);
+}
+
+decorate(TripStore, {
+  trips: observable,
+  loading: observable,
+});
+
+const tripStore = new TripStore();
+tripStore.fetchTrips();
+export default tripStore;
