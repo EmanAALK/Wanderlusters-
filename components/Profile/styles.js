c69305f28dc3296df9b34737c66cce5149f2ed84
assadribhi <assadribhi@gmail.com>
2020-08-31 16:17:48 +0300

    Initial FE userProfile

---
+++ components/Profile/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components/native";
+
+export const TopStyling = styled.View`
+  height: 15%;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  color: black;
+  font-size: 38;
+  text-align: center;
+`;
+
+export const OverLayContainer = styled.View`
+  flex: 1;
+  /* background-color: rgba(100, 40, 60, 0.4); */
+`;
