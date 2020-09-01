e88b2fe8395a58bf6a4eb77dbf249772177c4303
assadribhi <assadribhi@gmail.com>
2020-09-01 21:57:11 +0300

    cleanup and removed the Tab Navigator

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -6,9 +6,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Home from "../Home";
 import TripList from "../TripList";
 import TripDetail from "../TripList/TripDetail";
-
 import TripModal from "../modal/TripModal";
-
 import Signin from "../authentication/Signin";
 import Signup from "../authentication/Signup";
 import ProfileDetail from "../ProfileList/ProfileDetail";
