42526e5aaa95418498ea901d473005a282dd9f3f
assadribhi <assadribhi@gmail.com>
2020-09-01 18:36:45 +0300

    Profile to show a list of trips

--- components/ProfileList/ProfileDetail.js
+++ components/ProfileList/ProfileDetail.js
@@ -1,19 +1,27 @@
 import React from "react";
 import { observer } from "mobx-react";
-import { ListItem, Left, Text, Spinner } from "native-base";
+import { ListItem, Left, Text, Spinner, List } from "native-base";
 import profileStore from "../../stores/ProfileStore";
+import tripStore from "../../stores/TripStore";
 
 const ProfileDetail = ({ route }) => {
   if (profileStore.loading) return <Spinner />;
 
   const { profile } = route.params;
 
+  const profileTrips = tripStore.trips.filter(
+    (trip) => profile.userId === trip.userId
+  );
+
   return (
-    <ListItem>
-      <Left>
-        <Text>{profile.bio}</Text>
-      </Left>
-    </ListItem>
+    <>
+      <ListItem>
+        <Left>
+          <Text>{profile.bio}</Text>
+        </Left>
+      </ListItem>
+      <List>{profileTrips}</List>
+    </>
   );
 };
 
