c1ca422d7b15f948d6f224d2da73b111ddc75a00
Dee Macbook <ce_dalalalrasheedi@hotmail.com>
2020-09-03 20:55:20 +0300

    added date picker

--- components/modal/CreateTripModal.js
+++ components/modal/CreateTripModal.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { observer } from "mobx-react";
-
+import DatePicker from "react-native-datepicker";
+import moment from "moment";
 //Styles
 import {
   ModalContainer,
@@ -21,7 +22,10 @@ const CreateTripModal = ({ navigation }) => {
     image: "",
   });
 
+  const format = moment().format("DD/MM/YYYY");
+
   const handleSubmit = async () => {
+    console.log("check", trip.date);
     await tripStore.createTrip(trip);
     navigation.replace("TripList");
   };
@@ -34,11 +38,35 @@ const CreateTripModal = ({ navigation }) => {
         placeholder="Trip Name"
         placeholderTextColor="#A6AEC1"
       />
-      <ModalTextInput
-        onChangeText={(date) => setTrip({ ...trip, date })}
-        placeholder="Date"
-        placeholderTextColor="#A6AEC1"
+
+      <DatePicker
+        style={{ width: 200 }}
+        date={trip.date}
+        mode="date"
+        placeholder="select date"
+        format="YYYY-MM-DD"
+        // minDate="2016-05-01"
+        // maxDate="2016-06-01"
+        confirmBtnText="Confirm"
+        cancelBtnText="Cancel"
+        customStyles={{
+          dateIcon: {
+            position: "absolute",
+            left: 0,
+            top: 4,
+            marginLeft: 0,
+          },
+          dateInput: {
+            marginLeft: 36,
+          },
+          // ... You can check the source to find the other keys.
+        }}
+        onDateChange={(date) => {
+          console.log("date", date);
+          return setTrip({ ...trip, date });
+        }}
       />
+
       <ModalTextInput
         // event handler is repeated
         onChangeText={(description) => setTrip({ ...trip, description })}
